8ac143edc4aef2da1dc5260b1e6a0c0e790158ca
Monisha <sakhamurimonisha@gmail.com>
2023-06-13 19:10:58 -0400

    Test

--- server/routes/post.js
+++ server/routes/post.js
@@ -5,43 +5,43 @@ const router = express.Router();
 
 // 2. create all routes to access database
 router
-.post('/getpost', async (req, res) => {
-    try {
-      let noteget = await post.getpost(req.body.id);
-      res.send(noteget)
-    } catch(err) {
-      res.status(401).send({message: err.message});
-    }
-  })
+    .post('/getpost', async (req, res) => {
+        try {
+            let retrievedPost = await post.getpost(req.body.id);
+            res.send(retrievedPost)
+        } catch (err) {
+            res.status(401).send({ message: err.message });
+        }
+    })
 
 
-  .post('/createpost', async (req, res) => {
-    try {
-      let postcreates = await post.createpost(req.body.id, req.body.postcontent);
-      res.send(postcreates)
-    } catch(err) {
-      res.status(401).send({message: err.message});
-    }
-  })
-  
+    .post('/createpost', async (req, res) => {
+        try {
+            let createdPost = await post.createpost(req.body.id, req.body.postcontent);
+            res.send(createdPost)
+        } catch (err) {
+            res.status(401).send({ message: err.message });
+        }
+    })
 
 
-  .post('/updatepost', async (req, res) => {
-    try {
-      let postupdates = await post.updatepost(req.body.id, req.body.postcontent);
-      res.send(postupdates)
-    } catch(err) {
-      res.status(401).send({message: err.message});
-    }
-  })
-  .post('/deletepost', async (req, res) => {
-    try {
-      let postdeletes= await post.deletepost(req.body.id);
-      res.send(postdeletes)
-    } catch(err) {
-      res.status(401).send({message: err.message});
-    }
-  })
+
+    .post('/updatepost', async (req, res) => {
+        try {
+            let updatedPost = await post.updatepost(req.body.id, req.body.postcontent);
+            res.send(updatedPost)
+        } catch (err) {
+            res.status(401).send({ message: err.message });
+        }
+    })
+    .post('/deletepost', async (req, res) => {
+        try {
+            let deletedPost = await post.deletepost(req.body.id);
+            res.send(deletedPost)
+        } catch (err) {
+            res.status(401).send({ message: err.message });
+        }
+    })
 
 // 3. export router for use in index.js
-module.exports=router;
+module.exports = router;
